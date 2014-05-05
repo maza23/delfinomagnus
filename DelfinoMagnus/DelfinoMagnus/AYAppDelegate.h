7e43e5e4f69726848964f68dd6b85f08d21053c6
Avadesh Kumar <send2er.avadesh@gmail.com>
2014-05-05 23:10:15 +0530

    Initlal Project creation commit

---
+++ DelfinoMagnus/DelfinoMagnus/AYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AYAppDelegate.h
+//  DelfinoMagnus
+//
+//  Created by Avadesh Kumar on 5/5/14.
+//  Copyright (c) 2014 Delfino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DelfinoMagnus/DelfinoMagnus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DelfinoMagnus
+//
+//  Created by Avadesh Kumar on 5/5/14.
+//  Copyright (c) 2014 Delfino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AYAppDelegate class]));
+    }
+}
